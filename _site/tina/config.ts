88a8dd1734581c20a5b6ec23599885d5f948f0a8
mermaid <mermaid@users.noreply.github.com>
2023-10-21 17:41:39 -0600

    adding in tina

---
+++ _site/tina/config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "e7992e36-c94d-4a37-b979-416c1e7fb279", // Get this from tina.io
+  token: "3af269c2ff11cebb619c4f46017fa9db1ae5e5fc", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "./",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "./",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "posts",
+        label: "Posts",
+        path: "_posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
